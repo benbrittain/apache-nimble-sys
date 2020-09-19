59d5d6023caa0d16e668020414a8f82e76a1a408
Ben Brittain <ben@brittain.org>
2020-09-19 10:42:09 -0400

    Adds assertion debug info

--- build.rs
+++ build.rs
@@ -66,7 +66,6 @@ fn main() {
         .file("mynewt-nimble/ext/tinycrypt/src/aes_encrypt.c")
         .file("mynewt-nimble/ext/tinycrypt/src/utils.c")
         .file("mynewt-nimble/ext/tinycrypt/src/ccm_mode.c")
-
         // TODO more services
         .include("mynewt-nimble/nimble/host/include") // ble host
         .include("mynewt-nimble/porting/npl/dummy/include") // semaphore.h
@@ -91,6 +90,9 @@ fn main() {
         .clang_arg("-Imynewt-nimble/porting/npl/linux/include") // console.h
         .clang_arg("-Imynewt-nimble/ext/tinycrypt/include") // tinycrypt
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        // These types need to be defined by the wrapping OS
+        .blacklist_type("ble_npl_mutex")
+        // generate
         .generate()
         .expect("Unable to generate bindings");
 
@@ -99,5 +101,4 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
-
 }
